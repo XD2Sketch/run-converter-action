332b136f6b7545186c49d62e267300b011cbe72f
Andrey Chalkin <L2jLiga@gmail.com>
2020-11-11 19:50:25 +0700

    feat!: support esm executable

--- convert.js
+++ convert.js
@@ -4,42 +4,17 @@ const path = require('path')
 const util = require('util')
 const tmpDir = require('./tmp-dir')
 
+const execute = util.promisify(cp.exec);
+
 const executable = core.getInput('executable')
-const inputFileName = core.getInput('file-name')
+const fileName = core.getInput('file-name')
+const filePath = path.join(tmpDir.name, fileName);
 
-const MESSAGE_TEMPLATE = 'ERROR!\nfile: %s\ndescription: %s\nstacktrace: %s\nadditional info: %s'
-
-const runConverter = () => new Promise((resolve, reject) => {
-  const converter = cp.fork(executable, [
-    path.join(tmpDir.name, inputFileName),
-    '--experimental-symbols',
-  ])
-
-  converter.on('error', (error) => {
-    reject(error);
-    converter.kill();
-  });
-
-  converter.on('message', (msg) => {
-    const parsed = JSON.parse(msg)
-    if (parsed.type === 'error') {
-      reject(new Error(util.format(
-        MESSAGE_TEMPLATE,
-        inputFileName,
-        parsed.message,
-        parsed.stack,
-        parsed.additionalInfo || 'Not available',
-      )))
-      converter.kill()
-    } else {
-      console.log('received message: %O', parsed);
-    }
-  })
-
-  converter.on('exit', (code) => {
-    if (code === 0) resolve()
-    else reject()
-  })
-})
+const runConverter = async () => {
+  const { stderr } = execute(`node ${executable} ${filePath} --experimental-symbols`);
+  if (stderr && stderr.trim()) {
+    throw stderr;
+  }
+};
 
 module.exports.runConverter = runConverter
