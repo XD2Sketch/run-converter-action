ca3302a4727fe2d424dbbee00fe7e880ec702f10
Parth Jadhav <jadhavparth99@gmail.com>
2022-07-20 10:51:23 +0530

    feat: add F2S conversion
    fix: uploads and runners
    
    chore: rename conversion type
    
    chore: add conversion-type to required variables
    
    chore: refactor
    
    chore: build index.js
    
    chore: update file path (#3)
    
    chore: update paths (#4)

--- index.js
+++ index.js
@@ -6,20 +6,45 @@ const aws = require('./aws');
 const runner = require('./convert');
 const postMessage = require('./post-message');
 const tmpDir = require('./tmp-dir');
+const F2S_OUTPUT_DIR = 'output';
+
+const convertExtension = (filename, conversionType, extension) => {
+  if (conversionType === 'F2S') return `${filename}.${extension}`;
+  if (conversionType === 'XD2S') filename.replace(/\.xd$/, `.${extension}`)
+};
+
+const getFilePath = (filename, conversionType) => { 
+  if (conversionType === 'F2S') return filename;
+  if (conversionType === 'XD2S') return path.join(tmpDir.name, inputFileName);
+};
 
 const inputFileName = core.getInput('file-name');
 const awsOutputDir = core.getInput('aws-output-directory');
-const outputFileName = inputFileName.replace(/\.xd$/, `.${awsOutputDir}`);
-const awsFileName = `${github.context.issue.number}_${outputFileName}`;
+const conversionType = core.getInput('conversion-type');
 const messageEnabled = core.getInput('post-message-enabled') === '1';
+const executable = core.getInput('executable');
 
-aws.getFile()
+const outputFileName = convertExtension(inputFileName, conversionType, awsOutputDir);
+const awsFileName = `${github.context.issue.number}_${outputFileName}`;
+const filePath = getFilePath(inputFileName, conversionType);
+
+if (conversionType === 'F2S') {
+  runner.runConverter(executable, filePath)
+  .then(() => console.log(`"${inputFileName}" successfully converted to ${awsOutputDir}`))
+  .then(() => fs.readFileSync(path.join(tmpDir.name, F2S_OUTPUT_DIR, outputFileName)))
+  .then((data) => aws.uploadFile(awsFileName, data))
+  .then(() => console.log(`"${awsFileName}" successfully uploaded to AWS`))
+  .then(() => messageEnabled && postMessage(`Successfully converted ${inputFileName}\n\nResult available at: ${aws.getUrl(awsFileName)}`))
+  .catch((error) => core.setFailed(error));
+} else {
+  aws.getFile()
   .then((data) => fs.writeFileSync(path.join(tmpDir.name, inputFileName), data.Body))
   .then(() => console.log(`"${inputFileName}" is downloaded from AWS`))
-  .then(() => runner.runConverter())
+  .then(() => runner.runConverter(executable, filePath))
   .then(() => console.log(`"${inputFileName}" successfully converted to ${awsOutputDir}`))
   .then(() => fs.readFileSync(path.join(tmpDir.name, outputFileName)))
   .then((data) => aws.uploadFile(awsFileName, data))
   .then(() => console.log(`"${awsFileName}" successfully uploaded to AWS`))
   .then(() => messageEnabled && postMessage(`Successfully converted ${inputFileName}\n\nResult available at: ${aws.getUrl(awsFileName)}`))
   .catch((error) => core.setFailed(error));
+}
