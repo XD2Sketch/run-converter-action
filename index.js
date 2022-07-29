75a7a550d69472d7fd09438133f50b9dfa7f7bc7
Parth Jadhav <jadhavparth99@gmail.com>
2022-07-29 21:35:40 +0530

    chore: refactor

--- index.js
+++ index.js
@@ -7,18 +7,25 @@ const runner = require('./convert');
 const postMessage = require('./post-message');
 const tmpDir = require('./tmp-dir');
 
+const convertExtension = (filename, conversionType, extension) => {
+  if (conversionType === 'F2S') return `${filename}.${extension}`;
+  if (conversionType === 'XD2S') filename.replace(/\.xd$/, `.${extension}`)
+};
+
+const getFilePath = (filename, conversionType) => { 
+  if (conversionType === 'F2S') return filename;
+  if (conversionType === 'XD2S') return path.join(tmpDir.name, inputFileName);
+};
+
 const inputFileName = core.getInput('file-name');
 const awsOutputDir = core.getInput('aws-output-directory');
 const conversionType = core.getInput('conversion-type');
-let outputFileName = inputFileName.replace(/\.xd$/, `.${awsOutputDir}`);
-let filePath = path.join(tmpDir.name, inputFileName);
-if (conversionType === 'F2S') {
-  outputFileName = `${inputFileName}.sketch`;
-  filePath = inputFileName;
-}
-const awsFileName = `${github.context.issue.number}_${outputFileName}`;
 const messageEnabled = core.getInput('post-message-enabled') === '1';
 const executable = core.getInput('executable');
+
+const outputFileName = convertExtension(inputFileName, conversionType, awsOutputDir);
+const awsFileName = `${github.context.issue.number}_${outputFileName}`;
+const filePath = getFilePath(inputFileName, conversionType);
 
 if (conversionType === 'F2S') {
   runner.runConverter(executable, filePath)
