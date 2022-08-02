35c453d571e412daa13aa0b64b5018636d2053df
Parth Jadhav <jadhavparth99@gmail.com>
2022-08-02 16:28:00 +0530

    chore: console.log paths

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ const filePath = getFilePath(inputFileName, conversionType);
 if (conversionType === 'F2S') {
   runner.runConverter(executable, filePath)
   .then(() => console.log(`"${inputFileName}" successfully converted to ${awsOutputDir}`))
+  .then(() => console.log(fs.readdirSync(tmpDir)))
   .then(() => fs.readFileSync(path.join(tmpDir.name, outputFileName)))
   .then((data) => aws.uploadFile(awsFileName, data))
   .then(() => console.log(`"${awsFileName}" successfully uploaded to AWS`))
