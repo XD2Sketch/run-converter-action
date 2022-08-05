084a4d72b3b622105fb86bff463b2ca05710761e
Parth Jadhav <jadhavparth99@gmail.com>
2022-08-05 13:52:18 +0530

    feat(MAG-1704): support f2s conversion

--- index.js
+++ index.js
@@ -7,19 +7,41 @@ const runner = require('./convert');
 const postMessage = require('./post-message');
 const tmpDir = require('./tmp-dir');
 
+const getOutputFileName = (inputFileName, conversionType, awsOutputDir) => {
+  if (conversionType === 'XD2S') return inputFileName.replace(/\.xd$/, `.${awsOutputDir}`);
+  if (conversionType === 'F2S') return `${inputFileName}.${awsOutputDir}`;
+}
+
+const getFilePath = (fileName) => {
+  if (conversionType === 'XD2S') return path.join(tmpDir.name, fileName);
+  if (conversionType === 'F2S') return fileName;
+}
+
 const inputFileName = core.getInput('file-name');
 const awsOutputDir = core.getInput('aws-output-directory');
-const outputFileName = inputFileName.replace(/\.xd$/, `.${awsOutputDir}`);
+const conversionType = core.getInput('conversion-type');
+const outputFileName = getOutputFileName(inputFileName, conversionType, awsOutputDir);
 const awsFileName = `${github.context.issue.number}_${outputFileName}`;
 const messageEnabled = core.getInput('post-message-enabled') === '1';
+const filePath = getFilePath(inputFileName);
 
-aws.getFile()
+if (conversionType === 'XD2S') {
+  aws.getFile()
   .then((data) => fs.writeFileSync(path.join(tmpDir.name, inputFileName), data.Body))
   .then(() => console.log(`"${inputFileName}" is downloaded from AWS`))
-  .then(() => runner.runConverter())
+  .then(() => runner.runConverter(filePath))
   .then(() => console.log(`"${inputFileName}" successfully converted to ${awsOutputDir}`))
   .then(() => fs.readFileSync(path.join(tmpDir.name, outputFileName)))
   .then((data) => aws.uploadFile(awsFileName, data))
   .then(() => console.log(`"${awsFileName}" successfully uploaded to AWS`))
   .then(() => messageEnabled && postMessage(`Successfully converted ${inputFileName}\n\nResult available at: ${aws.getUrl(awsFileName)}`))
   .catch((error) => core.setFailed(error));
+} else {
+  runner.runConverter(filePath)
+  .then(() => console.log(`"${inputFileName}" successfully converted to ${awsOutputDir}`))
+  .then(() => fs.readFileSync(path.join(tmpDir.name, outputFileName)))
+  .then((data) => aws.uploadFile(awsFileName, data))
+  .then(() => console.log(`"${awsFileName}" successfully uploaded to AWS`))
+  .then(() => messageEnabled && postMessage(`Successfully converted ${inputFileName}\n\nResult available at: ${aws.getUrl(awsFileName)}`))
+  .catch((error) => core.setFailed(error));
+}
