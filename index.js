7186e4a8ea3042c7228577c005635145e6d26d73
Parth Jadhav <jadhavparth99@gmail.com>
2022-08-15 15:35:39 +0530

    feat: change output path

--- index.js
+++ index.js
@@ -17,10 +17,16 @@ const getFilePath = (fileName) => {
   if (conversionType === 'F2S') return fileName;
 }
 
+const getOutputFilePath = (outputFileName, conversionType) => {
+  if (conversionType === 'XD2S') return path.join(tmpDir.name, outputFileName);
+  if (conversionType === 'F2S') return path.join(tmpDir.name, 'output', outputFileName);
+}
+
 const inputFileName = core.getInput('file-name');
 const awsOutputDir = core.getInput('aws-output-directory');
 const conversionType = core.getInput('conversion-type');
 const outputFileName = getOutputFileName(inputFileName, conversionType, awsOutputDir);
+const outputFilePath = getOutputFilePath(outputFileName, conversionType);
 const awsFileName = `${github.context.issue.number}_${outputFileName}`;
 const messageEnabled = core.getInput('post-message-enabled') === '1';
 const filePath = getFilePath(inputFileName);
@@ -32,7 +38,7 @@ if (conversionType === 'XD2S') {
   .then(() => console.log(`"${inputFileName}" is downloaded from AWS`))
   .then(() => runner.runConverter(filePath))
   .then(() => console.log(`"${inputFileName}" successfully converted to ${awsOutputDir}`))
-  .then(() => fs.readFileSync(path.join(tmpDir.name, outputFileName)))
+  .then(() => fs.readFileSync(path.join(tmpDir.name, outputFilePath)))
   .then((data) => aws.uploadFile(awsFileName, data))
   .then(() => console.log(`"${awsFileName}" successfully uploaded to AWS`))
   .then(() => messageEnabled && postMessage(`Successfully converted ${inputFileName}\n\nResult available at: ${aws.getUrl(awsFileName)}`))
@@ -41,7 +47,7 @@ if (conversionType === 'XD2S') {
   console.log('Converting figma file to sketch file');
   runner.runConverter(filePath)
   .then(() => console.log(`"${inputFileName}" successfully converted to ${awsOutputDir}`))
-  .then(() => fs.readFileSync(path.join(tmpDir.name, outputFileName)))
+  .then(() => fs.readFileSync(path.join(tmpDir.name, outputFilePath)))
   .then((data) => aws.uploadFile(awsFileName, data))
   .then(() => console.log(`"${awsFileName}" successfully uploaded to AWS`))
   .then(() => messageEnabled && postMessage(`Successfully converted ${inputFileName}\n\nResult available at: ${aws.getUrl(awsFileName)}`))
